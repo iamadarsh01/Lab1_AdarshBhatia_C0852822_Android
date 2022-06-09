2ef579b835bdd677812a64afcfa997bc8895b3a4
iamadarsh01 <c0852822@mylambton.ca>
2022-06-08 20:28:38 -0400

    commit

--- app/src/main/java/com/example/androidtest1v3/MapsActivity.java
+++ app/src/main/java/com/example/androidtest1v3/MapsActivity.java
@@ -1,21 +1,42 @@
 package com.example.androidtest1v3;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import com.example.androidtest1v3.databinding.ActivityMapsBinding;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.androidtest1v3.databinding.ActivityMapsBinding;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+
+    private Marker newyork,a,b,c,d;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +51,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
+
+
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -43,9 +67,73 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Newyork and move the camera
+        LatLng newyork = new LatLng(40.71, -74.00);
+        mMap.addMarker(new MarkerOptions().position(newyork).title("New York"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(newyork));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(newyork, 8f));
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull LatLng latLng) {
+                setMarker(latLng);
+
+            }
+        });
+
+
+
+
     }
+
+    public  void setMarker(LatLng latLng){
+
+      //  MarkerOptions options = new MarkerOptions();
+
+
+        MarkerOptions options = new MarkerOptions().position(latLng).title("A");
+     Marker a =   mMap.addMarker(options);
+     drawLine();
+
+
+
+
+    }
+
+    public  void drawLine(){
+        PolylineOptions options = new PolylineOptions().
+                color(Color.GREEN)
+                .width(10)
+                .clickable(true)
+                .width(10)
+                .add(a.getPosition(), newyork.getPosition());
+        mMap.addPolyline(options);
+
+
+    }
+
+    public static String getMarkerTitle(int count){
+        switch (count){
+            case 1:
+                return "A";
+            case 2 :
+                return "B";
+            case 3 :
+                return "c";
+
+            case 4 :
+                return "D";
+
+
+            default:
+                return "";
+        }
+
+    }
+
+
+
+
+
+
 }
